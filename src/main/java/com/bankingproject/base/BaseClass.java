1ad06725a98112d555327910c7483edf9f099947
Nishant Patil <nishantpatil99@yahoo.com>
2023-01-18 21:51:37 +0530

    my first commit

---
+++ src/main/java/com/bankingproject/base/BaseClass.java
@@ -0,0 +1,17 @@
+package com.bankingproject.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Software Testing\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://demo.guru99.com/V1/index.php");
+
+ }
+}
